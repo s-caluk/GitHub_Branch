a12535cb6d3907c686ba3543c0fd49ba633d4f49
s-caluk <seher.caluk@gmail.com>
2023-01-14 00:35:20 +0100

    cakisma öncesi V8

--- src/B07.java
+++ src/B07.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class B07 {
+
+
+
+public class B07 {
+    public static void main(String[] args) {
+
+        /*
+        Farkli branchlerde calisirken , kendi projemiz oldugu icin
+        farkli classlarda calisip , cakisma olmasini engelledik.
+
+        ama bazen ister istemez cakismalar olabilir.
+
+         */
+    }
 }
