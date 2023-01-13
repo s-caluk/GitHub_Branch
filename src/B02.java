5a1818cdf0db31c84e9a8f4fa248d00e5c0caa76
s-caluk <seher.caluk@gmail.com>
2023-01-13 22:19:21 +0100

    V02

---
+++ src/B02.java
@@ -0,0 +1,25 @@
+
+
+public class B02 {
+    public static void main(String[] args) {
+
+        /*
+        Temelde git ve gitHub icin 3 asamadan bahsedebiliriz.
+
+        1- projemiz ile git´i tanistirma
+            git init
+
+        2- projemiz icin versiyon olusturma
+            git add .
+            git commit -m "versiyon adi"
+
+        3- gitHub ile entegrasyon ve senkronizasyon
+
+            - gitHubda yeni  repo olusturacagiz
+            - her degisiklik yaptigimizda -> git push
+
+         */
+
+    }
+
+}
