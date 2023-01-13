2f0208d09679f56b0ee1614ee4e5e50e06ccac89
s-caluk <seher.caluk@gmail.com>
2023-01-13 22:22:22 +0100

    V01-ilk commit

--- src/B01.java
+++ src/B01.java
@@ -3,5 +3,15 @@
 public class B01 {
     public static void main(String[] args) {
         System.out.println("GitHub son tekrar ve branch yapisi");
+
+        /*
+        intelJ > File > new > Project From Version Control --> uzak repodaki projeyi senin intelJ ekliyor.
+        Eklemeler yapip projeye , senin github da yeni repo olusturup, oraya atabilirsin
+        git pull          => uzak repodaki güncellemeri sana getirir.
+        git log           => loglari detayli verir
+        git log --oneline => loglari özet verir
+        git checkout nr.  => o numaradaki versiyona götürür, bakip cikarsin.
+        git revert nr.    => o numaradaki veryiyona kalici götürür, sonra commit yapmalisin.
+         */
     }
 }
